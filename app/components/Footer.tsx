fa59edcb89fc8fb59923b1ce491d9461e45b61b9
Rodrigo Medeiros <69992252+Rodrigo-171@users.noreply.github.com>
2024-02-26 18:11:53 -0300

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer(){
     return(
         <footer className='bg-zinc-800 border-t border-zinc-700 px-6 py-4 flex items-center justify-between'>
         <div className='flex items-center gap-3'>
-          <Image src="/images/album.jpg" width={48} height={48} alt="Capa do album de system of down" />
+          <Image src="./images/album.jpg" width={48} height={48} alt="Capa do album de system of down" />
           <div className='flex flex-col'>
             <strong className='font-normal'>Chop Suey!</strong>
             <span className='text-xs text-zinc-400'>System of Down</span>
