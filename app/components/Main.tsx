b2396f32a1084bb4d8c8b9122f4c1bcf06a7400c
Rodrigo Medeiros <69992252+Rodrigo-171@users.noreply.github.com>
2024-02-26 13:07:55 -0300

    Update Main.tsx

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -17,42 +17,42 @@ export function Main(){
 
           <div className='grid grid-cols-3 gap-6 mt-4'>
             <a href="#" className='bg-white/5 group flex items-center gap-4  rounded overflow-hidden hover:bg-white/10 transition-colors'>
-              <Image src="/images/album.jpg" width={104} height={104} alt="Capa do album de system of down" />
+              <Image src="./images/album.jpg" width={104} height={104} alt="Capa do album de system of down" />
               <strong>Chop Suey!</strong>
               <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible'>
                 <Play fill='bg-black'/>
               </button>
             </a>
             <a href="#" className='bg-white/5 group flex items-center gap-4 rounded overflow-hidden hover:bg-white/10 transition-colors'>
-              <Image src="/images/album2.jpg" width={104} height={104} alt="Capa do album de system of down" />
+              <Image src="./images/album2.jpg" width={104} height={104} alt="Capa do album de system of down" />
               <strong>Psychosocial</strong>
               <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible'>
                 <Play fill='bg-black'/>
               </button>
             </a>
             <a href="#" className='bg-white/5 group flex items-center gap-4 rounded overflow-hidden hover:bg-white/10 transition-colors'>
-              <Image src="/images/album3.jpg" width={104} height={104} alt="Capa do album de system of down" />
+              <Image src="./images/album3.jpg" width={104} height={104} alt="Capa do album de system of down" />
               <strong>A Little Piece of Heaven</strong>
               <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible'>
                 <Play fill='bg-black'/>
               </button>
             </a>
             <a href="#" className='bg-white/5 group flex items-center gap-4 rounded overflow-hidden hover:bg-white/10 transition-colors'>
-              <Image src="/images/album4.jpg" width={104} height={104} alt="Capa do album de system of down" />
+              <Image src="./images/album4.jpg" width={104} height={104} alt="Capa do album de system of down" />
               <strong>Um Minuto Para o Fim do Mundo</strong>
               <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible'>
                 <Play fill='bg-black'/>
               </button>
             </a>
             <a href="#" className='bg-white/5 group flex items-center gap-4 rounded overflow-hidden hover:bg-white/10 transition-colors'>
-              <Image src="/images/album5.jpg" width={104} height={104} alt="Capa do album de system of down" />
+              <Image src="./images/album5.jpg" width={104} height={104} alt="Capa do album de system of down" />
               <strong>Pelados em Santos</strong>
               <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible'>
                 <Play fill='bg-black'/>
               </button>
             </a>
             <a href="#" className='bg-white/5 group flex items-center gap-4 rounded overflow-hidden hover:bg-white/10 transition-colors'>
-              <Image src="/images/album6.jpg" width={104} height={104} alt="Capa do album de system of down" />
+              <Image src="./images/album6.jpg" width={104} height={104} alt="Capa do album de system of down" />
               <strong>Bohemian Rhapsody</strong>
               <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible'>
                 <Play fill='bg-black'/>
